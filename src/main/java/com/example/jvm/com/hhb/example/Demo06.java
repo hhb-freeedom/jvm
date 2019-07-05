93ebcc0b8528c1a7f54f97f6985557827803ff50
huanghongbo <huanghongbo@baiwang.com>
2019-07-05 11:17:48 +0800

    jvm

---
+++ src/main/java/com/example/jvm/com/hhb/example/Demo06.java
@@ -0,0 +1,24 @@
+package com.example.jvm.com.hhb.example;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author: huanghongbo
+ * @Date: 2019-07-04 16:41
+ * @Description:
+ */
+public class Demo06 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        while (true) {
+            Thread.sleep(3000);
+            int i = 0;
+            Demo06 demo06 = new Demo06();
+        }
+
+    }
+
+
+}
